c0e9444802f4e1b09d81a4813833e25796b33665
Camelia Lazar <camelia.lazar18@gmail.com>
2022-03-15 15:26:43 +0200

    Added files

--- SSVV_ProjectLab/src/main/java/Main.java
+++ SSVV_ProjectLab/src/main/java/Main.java
@@ -12,9 +12,9 @@ public class Main {
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
 
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "src/main/resources/studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "src/main/resources/teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "src/main/resources/note.xml");
 
         Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         UI consola = new UI(service);
